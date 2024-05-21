87faa847670c31e7b77c579e4bc0f9917a0b10c4
Utkarsh1407 <utkarshkumar5229@gmail.com>
2024-05-21 09:34:00 +0530

    learned variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accoundId= 14435
+let accountEmail= "hitesh@google.com"
+var accountPass= "12345"
+accountcity="Jaipur"
+let accountState;
+
+// accountId=2  not allowed
+
+accountEmail="hc@hc.com"
+accountPass="212121"
+accountcity="Bengaluru"
+
+console.log(accoundId);
+console.table([accoundId,accountEmail,accountPass,accountcity,accountState])

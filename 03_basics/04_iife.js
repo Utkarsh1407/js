084d8a13d19d648376fb205764c48ae74ad6b015
Utkarsh1407 <utkarshkumar5229@gmail.com>
2024-05-27 11:47:10 +0530

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
+
+// **** to stop the problems of globel scope pollution ******
